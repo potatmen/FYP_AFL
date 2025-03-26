2f9450d46d4c91a574c21af644aeb416e700d6f9
potatmen <ashatokhi2-c@my.cityu.edu.hk>
2025-03-26 19:40:49 +0800

    trial

--- llvm_mode/afl-llvm-pass.so.cc
+++ llvm_mode/afl-llvm-pass.so.cc
@@ -140,18 +140,18 @@ void AFLCoverage::distribute(int v, const set<int> &available){
 bool AFLCoverage::runOnModule(Module &M) {
 
 
-  const char* env_var_fuzz_num = "FUZZERS";
+ // const char* env_var_fuzz_num = "FUZZERS";
 
-  const char* env_var_fuzz_num_value = getenv(env_var_fuzz_num);
-
-  int numberOfFuzzers = atoi(env_var_fuzz_num_value);
+  //const char* env_var_fuzz_num_value = getenv(env_var_fuzz_num);
+  int numberOfFuzzers = 1;
+  //int numberOfFuzzers = atoi(env_var_fuzz_num_value);
   
 
-  const char* env_var_fuzz_id = "FUZZERID";
+  //const char* env_var_fuzz_id = "FUZZERID";
 
-  const char* env_var_fuzz_id_value = getenv(env_var_fuzz_id);
-
-  int fuzzerId = atoi(env_var_fuzz_id_value);
+  //const char* env_var_fuzz_id_value = getenv(env_var_fuzz_id);
+  int fuzzerId = 1;
+  //int fuzzerId = atoi(env_var_fuzz_id_value);
   
 
   ACTF("Number of fuzzers is %d and fuzzer id is %d", numberOfFuzzers, fuzzerId);
